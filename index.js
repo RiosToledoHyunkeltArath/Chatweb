8230a7e4f584d96a4e6079d56b9a001077d06c0a
RiosToledoHyunkeltArath <hriost2000@alumno.ipn.mx>
2022-11-25 15:13:02 -0600

    chatweb

---
+++ index.js
@@ -0,0 +1,62 @@
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./helpers/formatDate')
+const {
+  getActiveUser,
+  exitRoom,
+  newUser,
+  getIndividualRoomUsers
+} = require('./helpers/userHelper');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = newUser(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // General welcome
+    socket.emit('message', formatMessage("Chat", 'Bienvenido al chat :)'));
+
+    // Current active users and room name
+    io.emit('roomUsers', {
+      users: getIndividualRoomUsers(user.room)
+    });
+  });
+
+  // Listen for client message
+  socket.on('chatMessage', msg => {
+    const user = getActiveUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = exitRoom(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage("Chat", `${user.username} salio de la sala`)
+      );
+
+      // Current active users and room name
+      io.emit('roomUsers', {
+        users: getIndividualRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
